2a2026bb3aaa5e28bae986341f29a1e6f7fb5904
Diogo Roman <diogo@linux-6owc.site>
2012-10-25 17:01:08 -0200

    patrimonio controller

--- controllers/patrimonies_controller.php
+++ controllers/patrimonies_controller.php
@@ -77,7 +77,7 @@ class PatrimoniesController extends AppController {
 		}
 		if (!empty($this->data)) {
 			if ($this->Patrimony->save($this->data)) {
-				$this->__setFlash('O Patrimonio foi modificado com sucesso','success');
+				$this->__setFlash('O Patrimonio foi modificado com sucesso','system-success');
 				$this->__goBack();
 			} else {
 				$this->__setFlash('The patrimony could not be saved. Please, try again.', 'error');
